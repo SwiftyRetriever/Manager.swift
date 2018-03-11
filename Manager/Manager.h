b63350485458b8b3bc9ffe426b3506561cda56da
SwiftRetriever <swift.retriever@gmail.com>
2018-03-11 16:49:28 +0800

    Inital Commit

---
+++ Manager/Manager.h
@@ -0,0 +1,19 @@
+//
+//  Manager.h
+//  Manager
+//
+//  Created by 张伟 on 2018/3/11.
+//  Copyright © 2018年 zevwings. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Manager.
+FOUNDATION_EXPORT double ManagerVersionNumber;
+
+//! Project version string for Manager.
+FOUNDATION_EXPORT const unsigned char ManagerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Manager/PublicHeader.h>
+
+
